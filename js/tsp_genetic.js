d04a54aba0aa1fc5fd54885a686f10d8528eedae
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-14 08:48:24 +0100

    GA - Calculate fitness, choose and draw best order

--- js/tsp_genetic.js
+++ js/tsp_genetic.js
@@ -1,13 +1,28 @@
 const citiesNum = 5;
-const populationNum = 10;
+const populationNum = 100;
 
 let cities = [];
+let order = [];
 let population = [];
+let fitness = [];
+
+bestDistance = Infinity;
+bestOrder = [];
+
+function calculateDistance(points, order) {
+    let distance = 0;
+
+    for (let i = 0; i < order.length - 1; i++) {
+        cityA = points[order[i]];
+        cityB = points[order[i + 1]];
+        let d = dist(cityA.x, cityA.y, cityB.x, cityB.y);
+        distance += d;
+    }
+    return distance;
+}
 
 function setup() {
     createCanvas(600, 600);
-
-    let order = [];
 
     for (let i = 0; i < citiesNum; i++) {
         let v = createVector(random(width), random(height));
@@ -18,6 +33,45 @@ function setup() {
     for (let i = 0; i < populationNum; i++) {
         population[i] = shuffle(order.slice()); // p5.js shuffle function
     }
-    console.log(population);
+
+    for (let i = 0; i < population.length; i++) {
+        let dist = calculateDistance(cities, population[i]);
+        if (dist < bestDistance) {
+            bestDistance = dist;
+            bestOrder = population[i];
+        }
+        fitness[i] = dist;
+    }
+
+}
+
+function draw() {
+    background(0);
+
+    // draw cities
+    fill(255);
+    for (let i = 0; i < cities.length; i++) {
+        ellipse(cities[i].x, cities[i].y, 4, 4);
+    }
+
+    // draw current cities order
+    stroke(255);
+    noFill();
+    beginShape();
+    for (let i = 0; i < order.length; i++) {
+        let index = order[i];
+        vertex(cities[index].x, cities[index].y);
+    }
+    endShape();
+
+    // draw best cities order
+    stroke(0, 255, 0);
+    noFill();
+    beginShape();
+    for (let i = 0; i < bestOrder.length; i++) {
+        index = bestOrder[i];
+        vertex(cities[index].x, cities[index].y);
+    }
+    endShape();
 
 }
